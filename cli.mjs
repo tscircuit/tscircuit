6fa5be652d9bf1e7e2a80ccf44008b22f3d725b9
seveibar <seveibar@gmail.com>
2024-03-19 07:15:47 -0700

    update @tscircuit/cli, fix cli to properly set tscircuit version

--- cli.mjs
+++ cli.mjs
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+import { createRequire } from "node:module"
+const require = createRequire(import.meta.url)
+
+// Emits node warning which is annoying
+// import packageJson from "./package.json" with { type: "json" }
 
 global.TSCIRCUIT_VERSION = require("./package.json").version
 
