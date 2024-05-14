708da32f616588bc8f854974dae0b2b69b35719b
seveibar <seveibar@gmail.com>
2024-05-13 18:43:00 -0700

    fix: export component hooks from react-fiber

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 export * from "@tscircuit/builder"
 export * as ReactFiber from "@tscircuit/react-fiber"
-export { createRoot, useRenderedElements } from "@tscircuit/react-fiber"
+export {
+  createRoot,
+  useRenderedElements,
+  useResistor,
+  useBug,
+  useCapacitor,
+  useInductor,
+  useDiode,
+  useLed,
+  createUseComponent,
+  createUseComponentWithZod,
+} from "@tscircuit/react-fiber"
 export * from "@tscircuit/pcb-viewer"
 export * from "@tscircuit/schematic-viewer"
 export * from "@tscircuit/layout"
