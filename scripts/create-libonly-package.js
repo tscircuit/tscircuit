0586539013d1f268707e96ddf3a4e59e2deda7a3
Severin Ibarluzea <seveibar@gmail.com>
2025-07-23 19:09:00 -0700

    create libonly version tag (#719)

---
+++ scripts/create-libonly-package.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env bun
+
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const packageJsonPath = path.join(__dirname, '..', 'package.json')
+const originalPackage = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+
+const libonlyPackage = {
+  ...originalPackage,
+  name: `${originalPackage.name}-libonly`,
+  dependencies: {
+    ...originalPackage.dependencies
+  },
+  files: originalPackage.files.filter(file => file !== 'cli.mjs')
+}
+
+delete libonlyPackage.dependencies['@tscircuit/cli']
+delete libonlyPackage.bin
+
+const libonlyPackageJsonPath = path.join(__dirname, '..', 'package-libonly.json')
+fs.writeFileSync(libonlyPackageJsonPath, JSON.stringify(libonlyPackage, null, 2))
+
+console.log('Created package-libonly.json')
