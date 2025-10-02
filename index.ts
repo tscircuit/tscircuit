731d67ddcc8fffb616fa494f66293e62d1351f8c
naaa760 <neh6a683@gmail.com>
2025-10-02 17:39:37 +0530

    export new file type handler and import functionality

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 export * from "@tscircuit/core"
 export * from "@tscircuit/eval"
 export type { ChipProps, PinLabelsProp, CommonLayoutProps } from "@tscircuit/props"
+
+export * from "./src/filetype-handlers"
+export * from "./src/file-import-loader"
+export * from "./src/bun-kicad-plugin"
