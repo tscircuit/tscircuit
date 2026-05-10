fc22e72c1acc2a36a63465636e10a9b64f22d3f4
Severin Ibarluzea <seveibar@gmail.com>
2026-05-09 18:29:44 -0700

    Add kicad-pcb types (#3162)

--- types/static-assets.d.ts
+++ types/static-assets.d.ts
@@ -13,6 +13,17 @@ declare module "*.kicad_mod" {
   export default src
 }
 
+declare module "*.kicad_pcb" {
+  import type { AnyCircuitElement } from "circuit-json"
+
+  export const Board: (props: any) => any
+  export const boardContentCircuitJson: AnyCircuitElement[]
+  export const circuitJson: AnyCircuitElement[]
+
+  const circuitJsonDefault: AnyCircuitElement[]
+  export default circuitJsonDefault
+}
+
 declare module "*.glb" {
   const src: string
   export default src
